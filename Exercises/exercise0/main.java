0154efc330ad54e160a2d43da0955cbeea215db7
WEIREN FENG <feng.weir@northeastern.edu>
2023-10-14 11:08:00 -0700

    Excercise0 final

--- Exercises/exercise0/main.java
+++ Exercises/exercise0/main.java
@@ -5,39 +5,39 @@ public class main {
         book book1 = new book("1");
         book book2 = new book("2");
         book book3 = new book("3");
-
+        System.out.println("");
         calendar cal1 = new calendar("1");
         calendar cal2 = new calendar("2");
         calendar cal3 = new calendar("3");
-
+        System.out.println("");
         desk desk1 = new desk("1");
         desk desk2 = new desk("2");
         desk desk3 = new desk("3");
-
+        System.out.println("");
         fan fan1 = new fan("1");
         fan fan2 = new fan("2");
         fan fan3 = new fan("3");
-
+        System.out.println("");
         lamp lamp1 = new lamp("1");
         lamp lamp2 = new lamp("2");
         lamp lamp3 = new lamp("3");
-
+        System.out.println("");
         laptop laptop1 = new laptop("1");
         laptop laptop2 = new laptop("2");
         laptop laptop3 = new laptop("3");
-
+        System.out.println("");
         monitor monitor1 = new monitor("1");
         monitor monitor2 = new monitor("2");
         monitor monitor3 = new monitor("3");
-
+        System.out.println("");
         mug mug1 = new mug("1");
         mug mug2 = new mug("2");
         mug mug3 = new mug("3");
-
+        System.out.println("");
         phone phone1 = new phone("1");
         phone phone2 = new phone("2");
         phone phone3 = new phone("3");
-
+        System.out.println("");
         smartWatch smartWatch1 = new smartWatch("1");
         smartWatch smartWatch2 = new smartWatch("2");
         smartWatch smartWatch3 = new smartWatch("3");
